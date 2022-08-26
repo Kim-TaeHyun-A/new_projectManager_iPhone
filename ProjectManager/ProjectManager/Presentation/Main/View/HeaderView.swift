d759ea5f2598c95f662898e5bf2ae9917385676e
Tiana <97kth01@naver.com>
2022-08-27 03:49:40 +0900

    feat: HeaderView 매직넘버 처리 및 iphone이면 isUserInteractionEnabled 활성화

--- ProjectManager/ProjectManager/Presentation/Main/View/HeaderView.swift
+++ ProjectManager/ProjectManager/Presentation/Main/View/HeaderView.swift
@@ -8,6 +8,26 @@
 import UIKit
 import RxSwift
 
+private enum Constant {
+    static let titleFont: UIFont = {
+        switch UIDevice.current.userInterfaceIdiom {
+        case .pad:
+            return .preferredFont(forTextStyle: .largeTitle)
+        default:
+            return .preferredFont(forTextStyle: .body)
+        }
+    }()
+    
+    static let labelRadius: CGFloat = {
+        switch UIDevice.current.userInterfaceIdiom {
+        case .pad:
+            return 20
+        default:
+            return 10
+        }
+    }()
+}
+
 final class HeaderView: UIView {
     private var viewModel: ProjectListViewModelProtocol?
     private let disposeBag = DisposeBag()
@@ -15,7 +35,7 @@ final class HeaderView: UIView {
     private let listTitleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .preferredFont(forTextStyle: .largeTitle)
+        label.font = Constant.titleFont
         label.sizeToFit()
         return label
     }()
@@ -23,7 +43,7 @@ final class HeaderView: UIView {
     let countLabel: UILabel = {
         let label = UILabel()
         label.layer.masksToBounds = true
-        label.layer.cornerRadius = 20
+        label.layer.cornerRadius = Constant.labelRadius
         label.textColor = .white
         label.text = "0"
         label.textAlignment = .center
@@ -38,6 +58,7 @@ final class HeaderView: UIView {
         
         super.init(frame: .zero)
         
+        setUpIsInteractive()
         setUpLayout()
         bind()
         setUpTitle(title: status.string)
@@ -46,6 +67,12 @@ final class HeaderView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpIsInteractive() {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            isUserInteractionEnabled = true
+        }
     }
     
     private func setUpLayout() {
